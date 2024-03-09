a175909a54fb6c96ac5767c5772ae95be291d035
Amalya1278 <138888323+Amalya1278@users.noreply.github.com>
2024-03-10 00:05:23 +0400

    Add files via upload

---
+++ Vector/vector.h
@@ -0,0 +1,40 @@
+#include<iostream>
+#ifndef VECTOR_H
+#define VECTOR_H
+#include <cstddef>
+
+template<typename T>
+class Vector{
+	public:
+   using value_type = T;
+   using size_type = std::size_t;
+   using referance = value_type&;
+   using const_referance = const value_type&;
+   using pointer = value_type*;
+   using const_pointer = const value_type*;
+   private:
+   T* arr;
+   size_type sizet;
+   size_type capacity;
+   void reallloc();
+	public:
+	Vector();
+	Vector(size_type);
+	Vector(const Vector<T>&);
+	 Vector <T>& operator=(const Vector <T>&);
+	~Vector();
+	void push_back(const_referance);
+	void push_front(value_type);
+	void pop_back();
+	void pop_front();
+	referance at(size_type);
+	void resize(size_type);
+	void insert(size_type,value_type);
+	void remove(size_type);
+	size_type size();
+	void clear();
+	bool empty();
+};
+#include "vector.cpp"
+#endif
+
