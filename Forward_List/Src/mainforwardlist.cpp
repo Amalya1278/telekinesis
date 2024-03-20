c9d1af22ded666f67bcd3f5d7f38a2a793c33e93
Amalya1278 <138888323+Amalya1278@users.noreply.github.com>
2024-03-20 23:59:04 +0400

    mainforwardlist.cpp

--- Forward_List/Src/mainforwardlist.cpp
+++ Forward_List/Src/mainforwardlist.cpp
@@ -2,16 +2,19 @@
 #include<iostream>
 int main(){
 	Forward_list<int> list;
-	list.push_front(2);
-	list.push_front(3);
+	list.push_back(2);
+	list.push_back(3);
 	list.push_back(5);
 	list.insert(2,8);
 	list.pop_back();
-	list.traverse();
-	list.remove(8);
+//	list.traverse();
+/*	list.remove(8);
 	list.remove(2);
 	list.remove(3);
-//	list.traverse();
+	std::cout<<" ";*/
+	Forward_list<int> list1;
+	list1=list;
+	list1.traverse();
 	list.empty();
 	return 0;
 }
