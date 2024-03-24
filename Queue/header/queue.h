e374432c0f61d3a752ed4f7454ca5ebcd475a53d
Amalya1278 <138888323+Amalya1278@users.noreply.github.com>
2024-03-24 22:44:18 +0400

    Add files via upload

---
+++ Queue/header/queue.h
@@ -0,0 +1,43 @@
+#include<iostream>
+#ifndef QUEUE_H
+#define QUEUE_H
+#include<vector>
+#include <initializer_list>
+template<typename T,typename Container=std::vector<T>>
+class Queue{
+	private:
+	Container arr;
+	public:
+   using value_type = T;
+   using size_type = std::size_t;
+   using reference = value_type&;
+   using const_reference = const value_type&;
+   using pointer = value_type*;
+   using const_pointer = const value_type*;
+	Queue();
+	Queue(std::initializer_list<T>);
+	Queue(const Queue<T>&);
+	Queue(Queue <T>&&);
+	Queue& operator=(const Queue&);
+	~Queue();
+	reference front();
+	void pop();
+	void push(value_type);
+	size_type size_s();
+	bool empty_s();
+	void swap( Queue&);
+};
+
+template<typename T, typename Container>
+bool operator<(const Queue<T, Container>&, const Queue<T, Container>& );
+template<typename T, typename Container>
+bool operator>(const Queue<T, Container>&, const Queue<T, Container>&);
+template<typename T, typename Container>
+bool operator==(const Queue<T, Container>&, const Queue<T, Container>&);
+template<typename T, typename Container>
+bool operator<=(const Queue<T, Container>&, const Queue<T, Container>&);
+template<typename T, typename Container>
+bool operator>=(const Queue<T, Container>&, const Queue<T, Container>&);
+#include "queue.cpp"
+#endif //QUEUE_H
+
