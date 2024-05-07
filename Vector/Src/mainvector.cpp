c624d87f64f50ce89f142f2c507bea319facdc6b
Amalya1278 <138888323+Amalya1278@users.noreply.github.com>
2024-05-08 00:52:26 +0400

    changed mainvector.cpp

--- Vector/Src/mainvector.cpp
+++ Vector/Src/mainvector.cpp
@@ -6,10 +6,12 @@ int main() {
     Vector<int> v;
     v.push_back(1);
     v.push_back(2);
+	
     v.push_back(3);
+//	v.pop_front();
     std::cout << std::endl;
    // v.pop_back();
-    v.push_front(5);
+  //  v.push_front(5);
     v.insert(1, 15);
   //  v.remove(2);
 
@@ -17,7 +19,11 @@ int main() {
         std::cout << v.at(i) << " ";
     }
     std::cout << std::endl;
+	Vector<int> a;
+	a.push_back(2);
+	a.push_back(8);
+	v<a;
+
 
     return 0;
 }
-
