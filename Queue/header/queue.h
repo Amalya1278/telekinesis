055b67889f112cbd6281f0d92655e4ba5b75a90c
Amalya1278 <138888323+Amalya1278@users.noreply.github.com>
2024-05-08 02:05:01 +0400

    Update queue.h

--- Queue/header/queue.h
+++ Queue/header/queue.h
@@ -1,10 +1,10 @@
 #include<iostream>
-#ifndef QUEUE_H
-#define QUEUE_H
+#ifndef STACK_H
+#define STACK_H
 #include<vector>
 #include <initializer_list>
 template<typename T,typename Container=std::vector<T>>
-class Queue{
+class Stack{
 	private:
 	Container arr;
 	public:
@@ -14,31 +14,26 @@ class Queue{
    using const_reference = const value_type&;
    using pointer = value_type*;
    using const_pointer = const value_type*;
-	Queue();
-	Queue(std::initializer_list<T>);
-	Queue(const Queue<T,Container>&);
-	Queue(Queue <T,Conatiner>&&);
-	Queue& operator=(const Queue&);
-        const Queue& operator=(Queue<T,Container>&&);
-	~Queue();
-	reference front();
-	void pop();
-	void push(value_type);
-	size_type size_s();
-	bool empty_s();
-	void swap( Queue&);
+	Stack();
+	Stack(std::initializer_list<T>);
+	Stack(const Stack&);
+	Stack(Stack&&);
+	const Stack& operator=(const Stack&);//
+	const Stack& operator=(Stack&&);//
+	~Stack();
+	reference top();//
+	const_reference top()const;
+	void pop();//
+	void push(const_reference);//
+	size_type size_s();//
+	bool empty_s();//
+	void swap( Stack&);//
+
+bool operator<( const Stack&);//
+bool operator>( const Stack&);//
+bool operator==( const Stack&);//
+bool operator<=( const Stack&);//
+bool operator>=( const Stack&);//
 };
-
-template<typename T, typename Container>
-bool operator<(const Queue<T, Container>&, const Queue<T, Container>& );
-template<typename T, typename Container>
-bool operator>(const Queue<T, Container>&, const Queue<T, Container>&);
-template<typename T, typename Container>
-bool operator==(const Queue<T, Container>&, const Queue<T, Container>&);
-template<typename T, typename Container>
-bool operator<=(const Queue<T, Container>&, const Queue<T, Container>&);
-template<typename T, typename Container>
-bool operator>=(const Queue<T, Container>&, const Queue<T, Container>&);
-#include "queue.cpp"
-#endif //QUEUE_H
-
+#include "stack.cpp"
+#endif //STACK_H
